3720b49cfda9b415c2aedb80e249d32351991bd2
tamyallo <95342250+tamyallo@users.noreply.github.com>
2022-04-06 10:15:07 +0200

    formulario2

--- assets/js/formularios2.js
+++ assets/js/formularios2.js
@@ -67,7 +67,7 @@ function validarFormulario( enviar ) {
     if (
         validarSoloTexto( nombre )
         //&& validarNumero( edad, 0, 120 )
-        //&& validarEmail( email )
+        && validarEmail( email )
         //&& validarTextarea( mensaje, 3, 255 )
         && confirm("¿Deseas enviar el formulario con estos datos?")
     ){
@@ -155,6 +155,11 @@ function mensajeError ( error, elemento="", min=0, max=300, id="errores" ) {
             etiquetaInfo.innerHTML = texto;
         break;
 
+        case 3:
+            texto += "El correo electrónico no parece un email válido";
+            etiquetaInfo.innerHTML = texto;
+        break;
+
         // default:
     }
 
@@ -216,47 +221,26 @@ function validarObligatorio( elemento ) {
     @return: Booleano
 */
 
-function validarSoloTexto( elemento ) {
+function validarEmail ( elemento ) {
 
-    // Espresión regular para aceptar sólo letras del alfabeto español
-    // y espacios en blanco
-
-    var expresionRegular = /^[a-zA-ZÑñáéíóúÁÉÍÓÚ\s]+$/;
-
-    // validarObligatorio() Escribe un mensaje de error en el DOM
-    // y devuelve true | false
+    var expresionRegular =/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/ ;
 
     var validacion = validarObligatorio( elemento );
 
     switch ( validacion ) {
 
-        // Si el valor de 'validacion' es TRUE significa que
-        // O no es obligagorio, o contiene algo
-
-        // Validación de campo obligatorio superada en case validacion = true
         case true:
-            // Se ejecuta la validación por expresión regular ( true | false )
+           
             var resultadoExpRegular = expresionRegular.exec( elemento.value );
 
-            // Si el resultado de la validación NO ES true
             if ( !resultadoExpRegular ) {
 
-                // Se ejecuta la función con el mensaje de error en DOM
-                // El mensajeError(2) inserta un mensaje de error en el DOM
-                // y retorna FALSE
-                validacion = mensajeError( 2, elemento );
-                // validacion = false;
+                validacion = mensajeError( 3, elemento );
+               
             }
         break;
 
-        // Si el valor de 'validacion' es FALSE no hay nada que hacer
-        // case false:
-        //     validacion = false
-        // break;
-
-        // default:
     }
 
-    // Se devuelve el resultado de la validación (true | false)
     return validacion;
 }
